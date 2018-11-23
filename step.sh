d905c87ca2c18f7bdf0e711d61464eedf01ab61f
Viktor Benei <viktor.benei@gmail.com>
2018-11-23 15:44:51 +0100

    Added implementation

--- step.sh
+++ step.sh
@@ -1,22 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# set env vars
+uname_platform="$(uname -s)"
+if [[ "$uname_platform" == 'Darwin' ]] ; then
+  export ANDROID_NDK_PLATFORM='darwin'
+elif [[ "$uname_platform" == 'Linux' ]] ; then
+  export ANDROID_NDK_PLATFORM='linux'
+fi
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+if [[ "$ANDROID_NDK_HOME" == "" ]] ; then
+  if [[ "$ANDROID_NDK_PLATFORM" == 'darwin' ]] ; then
+    export ANDROID_NDK_HOME="$HOME/Library/Android/sdk/ndk-bundle"
+  elif [[ "$ANDROID_NDK_PLATFORM" == 'linux' ]] ; then
+    export ANDROID_NDK_HOME="/opt/android-ndk"
+  else
+    echo "Unsupported ANDROID_NDK_PLATFORM: $ANDROID_NDK_PLATFORM"
+    exit 1
+  fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+  # expose for subsequent steps
+  envman add --key ANDROID_NDK_HOME --value "$ANDROID_NDK_HOME"
+fi
+
+
+# ------------------------------------------------------
+# --- Android NDK
+
+# clean up if a previous version is already installed
+mkdir -p "$ANDROID_NDK_HOME"
+rm -rf "$ANDROID_NDK_HOME"
+
+# download
+mkdir -p /tmp/android-ndk-tmp
+cd /tmp/android-ndk-tmp
+wget -q https://dl.google.com/android/repository/android-ndk-${android_ndk_version}-${ANDROID_NDK_PLATFORM}-x86_64.zip
+
+# uncompress
+unzip -q android-ndk-${android_ndk_version}-${ANDROID_NDK_PLATFORM}-x86_64.zip
+
+# move to its final location
+mv ./android-ndk-${android_ndk_version} ${ANDROID_NDK_HOME}
+
+# remove temp dir
+cd ${ANDROID_NDK_HOME}
+rm -rf /tmp/android-ndk-tmp
+
+
+# add to PATH
+export PATH=${PATH}:${ANDROID_NDK_HOME}
+# expose for subsequent steps
+envman add --key PATH --value "$PATH"
